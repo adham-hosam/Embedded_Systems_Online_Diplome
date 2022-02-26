eb89a686b494d210d239a76fc1f760b497ff563c
ADHAM <adhamhosam9@gmail.com>
2022-02-26 16:47:42 +0200

    Sending a string

---
+++ Embedded_C/lesson 3/lap 2/app.c
@@ -0,0 +1,10 @@
+#include "uart.h"
+unsigned char string1[100]="Learn_in_depth_ADHAM";
+unsigned const char string2[100]="Learn_in_depth_ADHAM";
+unsigned const char string3[100]="Learn_in_depth_ADHAM";
+
+void main(void)
+{
+	uart_string(string1);
+	
+}
